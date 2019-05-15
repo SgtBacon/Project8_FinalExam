b9a37d3cbcd5211669ee18cd8e843add4a6c9a52
dtucker21 <dtucker21@my.whitworth.edu>
2019-05-14 18:55:35 -0700

    calm giraffe

--- source/main.cpp
+++ source/main.cpp
@@ -11,27 +11,27 @@ int main() {
     Reactor test = Reactor();
     std::cout << "Reactor built." << std::endl;
     Jay.Add_FuelCell(test);
-    std::cout << "Reactor contains " << test.getAssembly().size() << " Fuel Cells." << std::endl;
-    test.getAssembly().pop_back();
-    std::cout << "Fuel Cell 5 compromised\n" << "Reactor contains " << test.getAssembly().size() << " Fuel Cells." << std::endl;
+    std::cout << "Reactor contains " << test.assembly.size() << " Fuel Cells." << std::endl;
+    test.assembly.pop_back();
+    std::cout << "Fuel Cell 5 compromised\n" << "Reactor contains " << test.assembly.size() << " Fuel Cells." << std::endl;
 
-    std::cout << "\nFuel Cell 1 contains " << test.getAssembly().at(0).rod.size() << " Pellets." << std::endl;
-    for (i = 0; test.getAssembly().at(0).rod.empty() != true; i++) {
+    std::cout << "\nFuel Cell 1 contains " << test.assembly.at(0).rod.size() << " Pellets." << std::endl;
+    for (i = 0; test.assembly.at(0).rod.empty() != true; i++) {
         dc = rand() % 20 + 1;
         if (i < 5) {
-            test.getAssembly().at(i).decay(dc);
+            test.assembly.at(i).decay(dc);
             std::cout << dc << std::endl;
         }
         else
             break;
     }
     std::cout << "It took " << i << " calls of decay() to empty Fuel Cell 1.\nThis produced " << 45 * i << " watts.\n" << std::endl;
-    while (!test.getAssembly().empty()) {
-        test.getAssembly().pop_back();
+    while (!test.assembly.empty()) {
+        test.assembly.pop_back();
     }
-    std::cout << test.getAssembly().size() << std::endl;
+    std::cout << test.assembly.size() << std::endl;
     for (int h = 0; h < 60; h++) {
-        test.Meltdown(test.getAssembly());
+        test.Meltdown(test.assembly);
     }
 
     return 0;
