9eaf2321afc20954d99e4446b6e5733e9b69ba28
dtucker21 <dtucker21@my.whitworth.edu>
2019-05-16 15:20:47 -0700

    Update Reactor.h

--- headers/Reactor.h
+++ headers/Reactor.h
@@ -11,13 +11,13 @@ class Reactor {
         int o_size;
     public:
         std::vector<FuelCell> assembly;
-        // std::vector<FuelCell> getAssembly() {
-        //     return assembly;
-        // }
 
         int get_cd() {
             return countdown;
         }
+        //Causes the reactor to explode, calculating blast radius and square mileage of the destruction
+        //Pre: int for the cooldown
+        //Post: message to console describing the range of the expplosion
         void Explode(int cd) {             //Worst outcome, blast radius in a certain radius based on Power of a Reactor Pellet
             if (cd == 0) {
                 double blast = blast_radius();
@@ -26,16 +26,19 @@ class Reactor {
                 std::cout << "That covers " << (double)blast << " square miles" << std::endl;
             }
         }
+        //Calculates the blast radius
+        //Pre: None
+        //Post: blast radius
         double blast_radius() {
             double radius;
             double pi = 3.14;
-            //std::cout << "Radius calculation 1" << std::endl;
             radius = assembly[0].rod[0].PowerOut() / 700;
-            //std::cout << "Radius 1 = " << radius << "\nRadius calculation 2" << std::endl;
             radius = radius * o_size;
-            //std::cout << "Radius 2 = " << radius << std::endl;
             return radius;
         }
+        //Counts down to reactor explosion
+        //Pre: Assembly vector from a Reactor
+        //Post: Messages to console telling user the countdown to detonation or that the reactor stabilizes
         void Meltdown(std::vector<FuelCell> asmbly) {
             if (asmbly.empty()) {
                 countdown--;
@@ -45,12 +48,17 @@ class Reactor {
                 countdown = 60;
                 std::cout << "Reactor stabilized" << std::endl;
             }
-            //std::cout << "Calling explode()" << std::endl;
             Explode(get_cd());
         }
+        //Adds a new fuelcell to the reactor
+        //Pre: FuelCell
+        //Post: FuelCell at the end of the vector
         void addCell(FuelCell fc) {
             assembly.push_back(fc);
         }
+        //No-arg Constructor
+        //Pre: None
+        //Post: New reactor object with 5 default fuelcells
         Reactor() {
             o_size = 5;
             assembly = std::vector<FuelCell>(5);
@@ -59,26 +67,28 @@ class Reactor {
                 assembly.push_back(FuelCell());
             }
         }
+        //Alternate Constructor
+        //Pre: int RodCount, int PelletCount, int PowerLevel
+        //Post: Reactor object with a number of fuelcells equal to RodCount, a number of pellets in each fuelcell equal to PelletCount, and a power potential per pellet equal to PowerLevel
         Reactor(int rods, int num, int pr) {
             pwr = pr;
             o_size = rods;
-            //std::cout << "initializing fuel cell" << std::endl;
             FuelCell fc = FuelCell(num, pr);
-            //std::cout << "rods: " << rods << std::endl;
             assembly = std::vector<FuelCell>(rods);
             for (int i = 0; i < rods; i++) {
-                //std::cout << "i: " << i << std::endl;
                 assembly.push_back(fc);
             }
-            //std::cout << "done\n";
         }
+        //Removes the last cell from the assembly
+        //Pre: Reactor to remove from
+        //Post: removed fuelcell from vector
         void removeCell(Reactor newReactor){
             for(int i = 0; i < newReactor.assembly[0].get_osize(); i++){
-                //std::cout << "Entered loop" << std::endl;
                 if(newReactor.assembly[i].rod.empty()){
-                    //newReactor.assembly.erase(assembly.begin() + i);
                     std::vector<Pellet> temp = std::vector<Pellet>(newReactor.assembly[i].rod.size());
                     temp.swap(newReactor.assembly[i].rod);
+                    newReactor.assembly[newReactor.assembly.size() - 1].rod.swap(temp); //pushes empty cell to end of assembly vector
+                    newReactor.assembly.pop_back(); //pops off empty cell
                     std::cout << "removed an empty cell" << std::endl;
                     break;
                     }
@@ -88,8 +98,12 @@ class Reactor {
                     }
             }
         }
+        //Returns the total power potential in the fuelcell
+        //Pre: fuelcell
+        //Post: powerpotential returned
     int PowerOutput(int fc){
         int PowerOutput = fc * pwr;
+        return PowerOutput;
 }
 
 };
